7f39f11899d55725711b79fff52bf4b4c94b60f7
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-07-22 22:55:58 +0100

    chore: initial setup

--- build.ts
+++ build.ts
@@ -1,3 +1,4 @@
+import { chmod } from "node:fs/promises";
 import { build } from "esbuild";
 
 const b = () =>
@@ -14,4 +15,5 @@ const b = () =>
     minify: false,
   });
 
-Promise.all([b()]);
+await Promise.all([b()]);
+await chmod('bin', 744)
